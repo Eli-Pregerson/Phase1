8273da950f472b2cd82d2199de669356d6310b14
Shathebean <shaheen@gmail.com>
2022-05-19 15:11:21 -0700

    Added random guy's eliminate function lol

--- recurapc.py
+++ recurapc.py
@@ -1,4 +1,5 @@
 from sympy import *
+from eliminate import eliminate
 
 # def readCFG(cfg, recurlist):
 #     """Takes in a dot file, and extracts a list of two entry lists, each of which
@@ -124,7 +125,13 @@ def calculateSystem(edgelist, recurlist):
                 expr = expr + x
             expr = (recurexpr**recurlist[int(startnode)]) * expr #recursion
         system += [expr - sym]
-    gamma = eliminate(system, symbs, symbols("V0")*x - firstnode)
+    print("yes")
+    print(system)
+    eq1 = symbols("V0")*x - firstnode
+    # gamma = eliminate(system+[eq1], symbs, eq1)
+    gamma = list(eliminate(system+[eq1], *symbs))[0]
+    print(gamma)
+    print("no")
     return gamma
     # solutions = nonlinsolve(system, symbs)
     # possibleGenFunc = []
@@ -226,28 +233,110 @@ def termPow(term, symb):
         print(type(term))
         return 0
 
+# def seek(eqs, do, sol=[], strict=True):
+#     from sympy.solvers.solvers import _invert as f
+#     from sympy.core.compatibility import ordered
+#     while do and eqs:
+#         for x in do:
+#             for e in eqs:
+#                 if not isinstance(e, Eq):
+#                     continue
+#                 i, d = f(e.lhs - e.rhs, x)
+#                 if d != x:
+#                     continue
+#                 break
+#             else:
+#                 if strict:
+#                     assert None  # no eq could be solved for x
+#                 continue
+#             sol.append((d, i))
+#             eqs.remove(e)
+#             break
+#         do.remove(x)
+#         if not strict:
+#             do.extend(i.free_symbols)
+#             do = list(ordered(do))
+#         for _ in range(len(eqs)):
+#             if not isinstance(eqs[_], Eq):
+#                 continue
+#             # avoid dividing by zero
+#             ln, ld = eqs[_].lhs.as_numer_denom()
+#             rn, rd = eqs[_].rhs.as_numer_denom()
+#             eqs[_] = Eq(ln*rd, rn*ld).xreplace({x: i})
+#             if eqs[_] == False:
+#                 raise ValueError('inconsistency detected')
+#     return sol
+#
+# def focus(eqs, *syms, **kwargs):
+#     """Given Equality instances in ``eqs``, solve for symbols in
+#     ``syms`` and resolve as many of the free symbols in the solutions
+#     as possible. When ``evaluate=True`` a dictionary with keys being
+#     ``syms`` is returned, otherwise a list of identified symbols
+#     leading to the desired symbols is given.
+#
+#     Examples
+#     ========
+#     >>> focus((Eq(a, b), Eq(b + 2, c)), a)
+#     {a: c - 2}
+#     >>> focus((Eq(a, b), Eq(b + 2, c)), a, evaluate=False)
+#     [(b, c - 2), (a, b)]
+#     """
+#     from sympy.solvers.solvers import _invert as f
+#     from sympy.core.compatibility import ordered
+#     evaluate = kwargs.get('evaluate', True)
+#     assert all(isinstance(i, Eq) for i in eqs)
+#     sol = []
+#     free = Tuple(*eqs).free_symbols
+#     do = set(syms) & free
+#     if not do:
+#         return sol
+#     eqs = list(eqs)
+#     seek(eqs, do, sol)
+#     assert not do
+#     for x, i in sol:
+#         do |= i.free_symbols
+#     do = list(ordered(do))  # make it canonical
+#     seek(eqs, do, sol, strict=False)
+#     if evaluate:
+#         while len(sol) > len(syms):
+#             x, s = sol.pop()
+#             for i in range(len(sol)):
+#                 sol[i] = (sol[i][0], sol[i][1].xreplace({x: s}))
+#         for i in reversed(range(1, len(syms))):
+#             x, s = sol[i]
+#             for j in range(i):
+#                 y, t = sol[j]
+#                 sol[j] = y, f(y - t.xreplace({x: s}), y)[0]
+#     if evaluate:
+#         sol = dict(sol)
+#     else:
+#         sol = list(reversed(sol))
+#     return sol
+
+# def eliminate(system, symbs, gamma):
+#     print(system, symbs, gamma)
+#     """Takes in a system of equations and gets the gamma function"""
+#     done = True
+#     for symb in symbs:
+#         if str(symb) in str(gamma):
+#             done = False
+#     if done:
+#         return gamma
+#     for i in range(len(symbs)):
+#         sub = system[i] + symbs[i]
+#         if str(symbs[i]) in str(gamma):
+#             gamma = expand(gamma.subs(symbs[i], sub))
+#     return eliminate(system, symbs, gamma)
 
 
-def eliminate(system, symbs, gamma):
-    """Takes in a system of equations and gets the gamma function"""
-    done = True
-    for symb in symbs:
-        if str(symb) in str(gamma):
-            done = False
-    if done:
-        return gamma
-    for i in range(len(symbs)):
-        sub = system[i] + symbs[i]
-        if str(symbs[i]) in str(gamma):
-            gamma = expand(gamma.subs(symbs[i], sub))
-    return eliminate(system, symbs, gamma)
-
-recurlist = [0,0,0,0,1,1,0,0]
-edgelist = [[0,1],[1,2],[2,3],[3,7],[2,4],[4,5],[5,6],[6,7]]
+# recurlist = [0,0,0,0,1,1,0,0]
+# edgelist = [[0,1],[1,2],[2,3],[3,7],[2,4],[4,5],[5,6],[6,7]]
 # recurlist = [0,0,0,0,1,0]
 # edgelist = [[0,1],[1,2],[2,3],[2,4],[3,5],[4,5]]
 # recurlist = [0,0,0,0,0,1,0]
 # edgelist = [[0,1],[1,2],[2,3],[3,4],[3,5],[4,6],[5,6]]
 #print(calculateSystem(edgelist, recurlist))
+recurlist = [0,0,0,0,0]
+edgelist = [[0,1],[1,2],[2,3],[3,1],[1,4]]
 
 print("Recursive APC: " + str(recurapc(edgelist, recurlist)))
