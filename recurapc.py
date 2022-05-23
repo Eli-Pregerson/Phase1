67e83242ede27c6dfea95f3cb0dc152143060846
Shathebean <shaheen@gmail.com>
2022-05-23 13:10:30 -0700

    Numerical approximation

--- recurapc.py
+++ recurapc.py
@@ -33,15 +33,32 @@ def recurapc(edgelist, recurlist):
             if type(factor) == Pow and factor.args[1] < 0:
                 denominator *= 1/factor
         denominator = expand(denominator)
-        print(denominator)
+        maxPow = 0
+        for term in denominator.args:
+            power = termPow(term, symbols("x"))
+            if power > maxPow:
+                maxPow = power
         rootsDict = roots(denominator)
-        print(roots(denominator))
-        exprs = []
-        symbs = set()
         numRoots = sum(rootsDict.values())
-        print(numRoots)
+        if numRoots < maxPow:
+            newRootsDict = {}
+            approxroots = nroots(denominator)
+            for root in approxroots:
+                found = False
+                for dictRoot in newRootsDict.keys():
+                    if abs(root-dictRoot)<10**(-12):
+                        newRootsDict[dictRoot] += 1
+                        break
+                if not found:
+                    newRootsDict[root] = 1
+            rootsDict = newRootsDict
+            numRoots = sum(rootsDict.values())
+        if numRoots < maxPow:
+            raise Exception("Can't find all the roots :(")
         coeffs = [0]*numRoots
         Tseries = series(genFunc, x, 0, numRoots)
+        exprs = []
+        symbs = set()
         if not type(Tseries) == Order:
             for term in series(genFunc, x, 0, numRoots).args:
                 if not type(term) == Order:
@@ -49,7 +66,6 @@ def recurapc(edgelist, recurlist):
                     if k == "x":
                         k = "1"
                     coeffs[termPow(term, x)] = int(k)
-        print(coeffs)
         for val in range(numRoots):
             expr = -coeffs[val]
             for rootindex, root in enumerate(rootsDict.keys()):
@@ -64,7 +80,7 @@ def recurapc(edgelist, recurlist):
 
         signal.signal(signal.SIGALRM, handler)
         signal.alarm(200)
-        print(exprs)
+
         try:
             solutions = solve(exprs)
         except:
@@ -211,123 +227,91 @@ def eliminate(system, symbs):
             system[count] = expand(eq.subs(symbs[-1], sub))
     return eliminate(system[:-1], symbs[:-1])
 
-#This comes from the 2022 paper and apc should be O(1.12^n)
 # recurlist = [0,0,0,0,1,1,0,0]
 # edgelist = [[0,1],[1,2],[2,3],[2,4],[3,7],[4,5],[5,6],[6,7]]
-
-#This comes from the 2022 paper and apc should be O(n/5)
+# print("This comes from the 2022 paper and apc should be O(1.12^n)")
+# print("APC: " + str(recurapc(edgelist, recurlist)))
+#
+#
 # recurlist = [0,0,0,0,1,0]
 # edgelist = [[0,1],[1,2],[2,3],[2,4],[3,5],[4,5]]
-
-#This comes from the 2015 paper and apc should be O(n/3)
+# print("This comes from the 2022 paper and apc should be O(n/5)")
+# print("APC: " + str(recurapc(edgelist, recurlist)))
+#
+#
 # recurlist = [0,0,0,0,0]
 # edgelist = [[0,1],[1,2],[2,3],[3,1],[1,4]]
-
-#This comes from the 2022 paper and apc should be O(1.15^n)
-#bin2dec = {{0, 1, 0, f}, {1, 2, 0, f}, {1, 3, 0, f}, {2, 6, 0, t}, {3,
-     # 4, 0, f}, {3, 5, 0, f}, {4, 6, 1, t}, {5, 6, 1, t}};
+# print("This comes from the 2015 paper and apc should be O(n/3)")
+# print("APC: " + str(recurapc(edgelist, recurlist)))
+#
+#
+# #bin2dec = {{0, 1, 0, f}, {1, 2, 0, f}, {1, 3, 0, f}, {2, 6, 0, t}, {3,
+#      # 4, 0, f}, {3, 5, 0, f}, {4, 6, 1, t}, {5, 6, 1, t}};
 # recurlist = [0,0,0,0,1,1,0]
 # edgelist = [[0,1],[1,2],[1,3],[2,6],[3, 4], [3,5], [4,6], [5,6]]
-
-#This comes from the 2022 paper and apc should be O(n/3)
-#crossSum = {{0, 1, 0, f}, {0, 2, 0, f}, {1, 3, 0, t}, {2, 3, 1, t}};
+# print("This comes from the 2022 paper and apc should be O(1.15^n)")
+# print("APC: " + str(recurapc(edgelist, recurlist)))
+#
+#
+# #crossSum = {{0, 1, 0, f}, {0, 2, 0, f}, {1, 3, 0, t}, {2, 3, 1, t}};
 # recurlist = [0,0,1,0]
 # edgelist = [[0,1],[0,2],[1,3],[2,3]]
-
-
-# recurlist = [0,0,0,0,0]
-# edgelist = [[0,1],[1,2],[2,3],[3,1],[1,4]]ecurlist = [0,0,0,0,1,1,0,0]
-# edgelist = [[0,1],[1,2],[2,3],[3,7],[2,4],[4,5],[5,6],[6,7]]
-# recurlist = [0,0,0,0,1,0]
-# edgelist = [[0,1],[1,2],[2,3],[2,4],[3,5],[4,5]]
-# recurlist = [0,0,0,0,0,1,0]
-# edgelist = [[0,1],[1,2],[2,3],[3,4],[3,5],[4,6],[5,6]]
-#print(gammaFunction(edgelist, recurlist))
-
-ecurlist = [0,0,0,0,1,1,0,0]
-# edgelist = [[0,1],[1,2],[2,3],[3,7],[2,4],[4,5],[5,6],[6,7]]
-# recurlist = [0,0,0,0,1,0]
-# edgelist = [[0,1],[1,2],[2,3],[2,4],[3,5],[4,5]]
-# recurlist = [0,0,0,0,0,1,0]
-# edgelist = [[0,1],[1,2],[2,3],[3,4],[3,5],[4,6],[5,6]]
-#print(gammaFunction(edgelist, recurlist))
-
-
-# recurlist = [0,0,0,0,0]
-# edgelist = [[0,1],[1,2],[2,3],[3,1],[1,4]]ecurlist = [0,0,0,0,1,1,0,0]
-# edgelist = [[0,1],[1,2],[2,3],[3,7],[2,4],[4,5],[5,6],[6,7]]
-# recurlist = [0,0,0,0,1,0]
-# edgelist = [[0,1],[1,2],[2,3],[2,4],[3,5],[4,5]]
-# recurlist = [0,0,0,0,0,1,0]
-# edgelist = [[0,1],[1,2],[2,3],[3,4],[3,5],[4,6],[5,6]]
-#print(gammaFunction(edgelist, recurlist))
-
-# recurlist = [0,0,0,0,0]
-# edgelist = [[0,1],[1,2],[2,3],[3,1],[1,4]]ecurlist = [0,0,0,0,1,1,0,0]
-# edgelist = [[0,1],[1,2],[2,3],[3,7],[2,4],[4,5],[5,6],[6,7]]
-# recurlist = [0,0,0,0,1,0]
-# edgelist = [[0,1],[1,2],[2,3],[2,4],[3,5],[4,5]]
-# recurlist = [0,0,0,0,0,1,0]
-# edgelist = [[0,1],[1,2],[2,3],[3,4],[3,5],[4,6],[5,6]]
-#print(gammaFunction(edgelist, recurlist))
-
-# recurlist = [0,0,0,0,0]
-# edgelist = [[0,1],[1,2],[2,3],[3,1],[1,4]]
-# recurlist = [0,0,0,0,0]
-# edgelist = [[0,1],[1,2],[2,3],[3,1],[1,4]]
-#This comes from the 2022 paper and apc should be O(n/3)
-#fact = {{0, 1, 0, f}, {0, 2, 0, f}, {1, 3, 0, t}, {2, 3, 1, t}};
-# recurlist = [0,0,1,0]
-# edgelist = [[0,1],[0,2],[1,3],[2,3]]
-
-#This comes from the 2022 paper and apc should be O(n/2)
-#power = {{0, 1, 0, f}, {0, 2, 0, f}, {1, 5, 0, t}, {2, 3, 0, f}, {2,
-#    4, 0, f}, {3, 5, 0, t}, {4, 5, 1, t}};
+# print("This comes from the 2022 paper and apc should be O(n/3)")
+# print("APC: " + str(recurapc(edgelist, recurlist)))
+#
+#
+#
+#
+# #power = {{0, 1, 0, f}, {0, 2, 0, f}, {1, 5, 0, t}, {2, 3, 0, f}, {2,
+# #    4, 0, f}, {3, 5, 0, t}, {4, 5, 1, t}};
 # recurlist = [0,0,0,0,1,0]
 # edgelist = [[0,1],[0,2],[1,5],[2,3],[2,4],[3,5],[4,5]]
-
-
-
-
-
-
-#This comes from the 2015 paper and apc should be 2^3 = 8
+# print("This comes from the 2022 paper and apc should be O(n/2)")
+# print("APC: " + str(recurapc(edgelist, recurlist)))
+#
+#
+#
+#
+#
+#
+#
 # recurlist = [0,0,0,0,0,0,0]
 # edgelist = [[0,1],[0,2],[1,2],[2,3],[2,4],[3,4],[4,5],[4,6],[5,6]]
-# print("3 if else sequence APC: " + str(recurapc(edgelist, recurlist)))
-
-#This comes from the 2015 paper and apc should be 3+1 = 4
+# print("This comes from the 2015 paper and apc should be 2^3 = 8")
+# print("APC: " + str(recurapc(edgelist, recurlist)))
+#
+#
+#
 # recurlist = [0,0,0,0,0,0,0]
 # edgelist = [[0,1],[0,6],[1,2],[1,5],[2,3],[2,4],[3,4],[4,5],[5,6]]
-# print("3 if else nested APC: " + str(recurapc(edgelist, recurlist)))
-
-#This comes from the 2015 paper and apc should be n^4
+# print("This comes from the 2015 paper and apc should be 3+1 = 4")
+# print("APC: " + str(recurapc(edgelist, recurlist)))
+#
+#
 # recurlist = [0,0,0,0,0]
 # edgelist = [[0,0],[0,1],[1,1],[1,2],[2,2],[2,3],[3,3],[3,4],[4,4]]
-# print("4 loop sequence APC: " + str(recurapc(edgelist, recurlist)))
-
-# #This comes from the 2015 paper and apc should be 3+1 = 4
-# recurlist = [0,0,0,0,0,0,0]
-# edgelist = [[0,1],[0,6],[1,2],[1,5],[2,3],[2,4],[3,4],[4,5],[5,6]]
-# print("3 if else nested APC: " + str(recurapc(edgelist, recurlist)))
+# print("This comes from the 2015 paper and apc should be n^4")
+# print("APC: " + str(recurapc(edgelist, recurlist)))
 #
-# #This comes from the 2015 paper and apc should be n^3
+#
 # recurlist = [0,0,0,0,0]
 # edgelist = [[0,0],[0,1],[1,1],[1,2],[2,2],[2,3],[3,3],[3,4]]
-# print("3 loop sequence APC: " + str(recurapc(edgelist, recurlist)))
+# print("This comes from the 2015 paper and apc should be n^3")
+# print("APC: " + str(recurapc(edgelist, recurlist)))
 #
-#This comes from the 2015 paper and apc should be b^n
-# recurlist = [0,0,0,0,0]
-# edgelist = [[0,1],[1,0],[1,2],[2,1],[2,3],[3,2],[3,4]]
-# print("3 loop nested APC: " + str(recurapc(edgelist, recurlist)))
-
+#
 # recurlist = [0,0,0,0,0,0,0,0,0]
 # edgelist = [[0,1],[1,2],[2,8],[1,3],[3,4],[3,5],[5,8],[4,6],[4,7],[6,8],[7,8]]
-#
-# recurlist = [0,0,0,0,0,0,0,0,0]
-# edgelist = [[0,1],[1,2],[2,3],[3,2],[2,4],[4,5],[5,6],[6,5],[5,7],[7,8]]
+# print("This comes from the 2015 paper and apc should be 4")
+# print("APC: " + str(recurapc(edgelist, recurlist)))
 
-recurlist = [0,0,0,0,0,0,0,0,0,0,0]
-edgelist = [[0,1],[1,2],[2,3],[2,4],[4,6],[4,5],[6,8],[6,9],[5,7],[8,10],[9,7],[7,2],[3,10]]
 
+recurlist = [0,0,0,0,0,0,0,0,0]
+edgelist = [[0,1],[1,2],[2,3],[3,2],[2,4],[4,5],[5,6],[6,5],[5,7],[7,8]]
+print("This comes from the 2015 paper and apc should be n^2")
 print("APC: " + str(recurapc(edgelist, recurlist)))
+
+# recurlist = [0,0,0,0,0,0,0,0,0,0,0]
+# edgelist = [[0,1],[1,2],[2,3],[2,4],[4,6],[4,5],[6,8],[6,9],[5,7],[8,10],[9,7],[7,2],[3,10]]
+# print("This comes from the 2015 paper and apc should be 1.17^n")
+# print("APC: " + str(recurapc(edgelist, recurlist)))
